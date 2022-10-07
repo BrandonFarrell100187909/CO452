b2bb1435f27edad8160f2c80546c8e7bb5c40d6b
BrandonFarrell100187909 <100187909@student.burnley.ac.uk>
2022-10-07 11:53:01 +0100

    program 2 adjustmenrs

--- week2/program2.java
+++ week2/program2.java
@@ -6,6 +6,27 @@ public enum grades
     NS , F , D , C , B , A;
 
     private final int value;
+    public final int GRADE_NS = 0;
+        public final int GRADE_F = 39;
+        public final int GRADE_D = 49;
+        public final int GRADE_C = 59;
+        public final int GRADE_B = 69;
+        public final int GRADE_A = 100;
+    
+        {
+        if(mark >= GRADE_NS && mark <= GRADE_F)
+            System.out.println("This is a failed attempt");
+        else if (mark >= GRADE_F && mark <= GRADE_D)
+            System.out.println("This is a pass");
+        else if (mark >= GRADE_D && mark <= GRADE_C)
+            System.out.println("This is a 2:2");
+        else if (mark >= GRADE_C && mark <= GRADE_B);
+            System.out.println("This is a 2:1");
+        if (mark >= GRADE_B && mark <= GRADE_A)
+            System.out.println("This is a 1st");
+        else
+            System.out.println("This is an invalid mark");
+        } 
 
     private Grades(int value)
     {
@@ -20,32 +41,11 @@ public enum grades
 
 {
     public static void main(String[] args);
-}
-
-    public final int GRADE_NS = 0;
-    public final int GRADE_F = 39;
-    public final int GRADE_D = 49;
-    public final int GRADE_C = 59;
-    public final int GRADE_B = 69;
-    public final int GRADE_A = 100;
 
     {
-    if(mark >= GRADE_NS && mark <= GRADE_F)
-        System.out.println("This is a failed attempt");
-    else if (mark >= GRADE_F && mark <= GRADE_D)
-        System.out.println("This is a pass");
-    else if (mark >= GRADE_D && mark <= GRADE_C)
-        System.out.println("This is a 2:2");
-    else if (mark >= GRADE_C && mark <= GRADE_B);
-        System.out.println("This is a 2:1");
-    if (mark >= GRADE_B && mark <= GRADE_A)
-        System.out.println("This is a 1st");
-    else
-        System.out.println("This is an invalid mark");
+         
     }
-
-
-
+}
 
 
 }
